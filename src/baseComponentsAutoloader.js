c4e15730e701ae0a3a438ffa118d3907f4115b16
Victor Hugo Batista <victor.batista@i9xp.com.br>
2020-04-14 18:21:08 -0300

    Add Basic__.vue components autoloader

---
+++ src/baseComponentsAutoloader.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import upperFirst from "lodash/upperFirst";
+import camelCase from "lodash/camelCase";
+
+export default {
+  autoloadComponents() {
+    const requireComponent = require.context(
+      "./components",
+      false,
+      /Base[A-Z]\w+\.(vue|js)$/
+    );
+
+    requireComponent.keys().forEach(fileName => {
+      const componentConfig = requireComponent(fileName)
+
+      const componentName = upperFirst(
+        camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, "$1"))
+      );
+
+      Vue.component(componentName, componentConfig.default || componentConfig);
+    });
+  }
+};
